03f7c76779c5b370cde943eb4f27017808064692
zaki <zaki@Zakintosh-Pro.local>
2018-12-18 13:09:53 +0600

    Initial commit

---
+++ src/main/java/nhfzaki/restfulinpeace/RestfulInPeaceApplication.java
@@ -0,0 +1,14 @@
+package nhfzaki.restfulinpeace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulInPeaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulInPeaceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/nhfzaki/restfulinpeace/RestfulInPeaceApplicationTests.java
@@ -0,0 +1,17 @@
+package nhfzaki.restfulinpeace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulInPeaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
